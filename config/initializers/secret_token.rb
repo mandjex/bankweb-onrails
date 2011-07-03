a886db4f3f83a74d9a60f62c886bd4a2f68bac98
Maxime <mandjex@gmail.com>
2011-07-03 11:07:01 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankwebOnrails::Application.config.secret_token = 'b7c3ff66760c2a9167c92132caf572249322383bb46c6f28f263f44d4502704c99948e7b2bd8296e8ea80dc5e69c8fdca6ea46a8e36feaa73d205f38cbf642d5'
